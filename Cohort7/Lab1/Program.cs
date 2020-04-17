459474665c415946cb4342c76a353c71fc36bcde
Marlo <marlo.baguyos@outlook.com>
2020-04-16 21:58:02 -0400

    update

--- Cohort7/Lab1/Program.cs
+++ Cohort7/Lab1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lab1
 {
@@ -14,6 +15,19 @@ namespace Lab1
 
             string full = p.FullName("Sergio", "Inzunza");
             Console.WriteLine(full);
+
+            p.Exc1();
+        }
+
+        void Exc1() {
+            List<int> numbers = new List<int>();
+            // 10 20 .. 1000
+            for (int i = 1; i < 101; i++){
+                numbers.Add(i * 10);
+            }
+            Console.WriteLine(numbers);
+
+
         }
 
         void NoReturns(){
@@ -29,12 +43,16 @@ namespace Lab1
             string n = "98";
             int res = int.Parse(n);
 
-            string[] names = new string[100];
+            string[] names = new string[999];
             names[0] = "asd";
             names[1] = "123123";
             names[6] = "ajklasl";
 
             //string [] [] cube = new string[99][];
+            List<string> List = new List<string>();
+            List.Add("asdasd");
+
+            System.Console.WriteLine("From array: " + name[0]);
         }
         string FullName(string first, string last) {
             return first + " " + last;
