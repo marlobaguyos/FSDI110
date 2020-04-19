d456bc47493fc8ba52b2fcbeccfb5c5ff96ac6ff
marlo <marlo.baguyos@outlook.com>
2020-04-19 14:07:50 -0400

    update

---
+++ Cohort7/ERP/Controllers/CatalogController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ERP.Models;
+
+namespace ERP.Controllers
+{
+
+  public class CatalogController : Controller {
+
+    public IActionResult Index() {
+      return View();
+    }
+    
+    public IActionResult Register() {
+      return View();
+    }
+    
+    // public IActionResult Catalog(){
+    //   return View();
+    // }
+  }
+
+}
