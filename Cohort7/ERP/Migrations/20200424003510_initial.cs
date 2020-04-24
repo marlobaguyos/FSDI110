5cff8305c5784f8f3449eeaefdb2747e368e07f4
Marlo <marlo.baguyos@outlook.com>
2020-04-23 20:48:56 -0400

    update

---
+++ Cohort7/ERP/Migrations/20200424003510_initial.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ERP.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Image = table.Column<string>(nullable: true),
+                    Make = table.Column<string>(nullable: true),
+                    Model = table.Column<string>(nullable: true),
+                    Year = table.Column<int>(nullable: false),
+                    Color = table.Column<string>(nullable: true),
+                    Seats = table.Column<int>(nullable: false),
+                    Price = table.Column<int>(nullable: false),
+                    Mileage = table.Column<int>(nullable: false),
+                    Condition = table.Column<string>(nullable: true),
+                    MPG = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+        }
+    }
+}
