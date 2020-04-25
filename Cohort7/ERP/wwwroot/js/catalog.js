60314abf19c67a80e0cbe6805bcd3e479ee4c59f
Marlo <marlo.baguyos@outlook.com>
2020-04-25 17:40:23 -0400

    update

--- Cohort7/ERP/wwwroot/js/catalog.js
+++ Cohort7/ERP/wwwroot/js/catalog.js
@@ -1,44 +1,69 @@
 function retrieveCatalog() {
-//url: /catalog/getCatalog
-$.ajax({
-    url: '/catalog/getCatalog',
-    type: 'GET',
-    success: function(res){
-        console.warn("From server", res);
-        // display cars on the HTML
-        for(let i=0; i<res.length; i++)
-        {
-            displayCar(res[i]);
+    //url: /catalog/getCatalog
+    $.ajax({
+        url: '/catalog/getCatalog',
+        type: 'GET',
+        success: function (res) {
+            console.warn("From server", res);
+            // display cars on the HTML
+            for (let i = 0; i < res.length; i++) {
+                displayCar(res[i]);
+            }
+        },
+        error: function (detail) {
+            console.error(detail);
         }
-    },
-    error: function(detail) {
-        console.error(detail);
-    }
-});
+    });
 
 }
 
-function displayCar(car){
+function displayCar(car) {
     //get container
 
     var container = $("#catalog");
 
     //create template/sntax
-    var sntx =  
+    var sntx =
     `<div class="item" id="${car.year}">
-    <img class="image" src="${car.image}">
-    <h4>${car.year} ${car.make} ${car.model}</h4>
-    <h6 class="item-price">Price: $${car.price}</h6>
-    <p>Condition: ${car.color}</p>
-    <p>Condition: ${car.condition}</p>
-    <p>Condition: ${car.seats}</p>
-    <p>Condition: ${car.mileage}</p>
-    <p>Condition: ${car.mpg}</p>
-    <div class="button-div">
-        <button class="btn btn-primary mb-2">Add to Cart</button>
-    </div>
-    
+        <div class="row featurette">
+            <div class="col-md-7">
+                <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span>
+                </h2>
+                <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod
+                    semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus
+                    commodo.</p>
+                <div class="button-div">
+                    <button class="btn btn-primary mb-2">Add to Cart</button>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <img class="image bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" src="${car.image}" width="500" height="500">
+            </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+            <div class="col-md-7 order-md-2">
+                <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for
+                        yourself.</span></h2>
+                <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis
+                    euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus,
+                    tellus ac cursus commodo.</p>
+                <div class="button-div">
+                    <button class="btn btn-primary mb-2">Add to Cart</button>
+                </div>
+            </div>
+            <div class="col-md-5 order-md-1">
+                <img class="image bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" src="${car.image}" width="500" height="500">
+            </div>
+        </div>
+
+        <hr class="featurette-divider">
+
     </div>`;
+
+    
     //     <h3>${car.year} ${car.make} ${car.model}</h3>
     //     image here
     // </div>`;
